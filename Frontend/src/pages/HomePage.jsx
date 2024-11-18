0ef5bffe5be2de6fd30898735d39ea3823ea937c
Harsh Sharma <harshsharma90866@gmail.com>
2024-11-18 20:30:32 +0530

    Add Image upload section

---
+++ Frontend/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ImageUpload from '../components/ImageUpload'
+
+function HomePage() {
+  return (
+    <>
+        <div>
+            <h1>This is an image processing web app build on React + Python </h1>
+            <ImageUpload />
+        </div>
+    </>
+  )
+}
+
+export default HomePage
